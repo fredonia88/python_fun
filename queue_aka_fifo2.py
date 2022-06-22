56a5ce013dfea5a2d8a09d3b5e1e6ec5a0cda384
Frederick Jones <fjjones88@gmail.com>
2022-06-22 11:55:54 -0500

    Create queue_aka_fifo2.py

---
+++ queue_aka_fifo2.py
@@ -0,0 +1,38 @@
+class QueueError(BaseException):  # Choose base class for the new exception.
+    pass
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def put(self, elem):
+        self.queue.append(elem)
+
+    def get(self):
+        if len(self.queue) > 0:
+            val = self.queue[0]
+            del self.queue[0]
+            return val
+        else:
+            raise QueueError()
+
+class SuperQueue(Queue):
+    def __init__(self):
+        Queue.__init__(self)
+    
+    def isempty(self):
+        if len(self.queue) == 0:
+            return True
+        else:
+            return False
+
+
+que = SuperQueue()
+que.put(1)
+que.put("dog")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Queue empty")
