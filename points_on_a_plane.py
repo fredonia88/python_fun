67c70b3867692021c8d8a1b8717961cd105c2359
Frederick Jones <fjjones88@gmail.com>
2022-06-27 23:05:34 -0500

    Create points_on_a_plane.py

---
+++ points_on_a_plane.py
@@ -0,0 +1,31 @@
+class Point:
+    def __init__(self, x = 0.0, y = 0.0):
+        self.__x = x
+        self.__y = y
+
+    def getx(self):
+        return self.__x
+
+    def gety(self):
+        return self.__y
+
+    def distance_from_xy(self, x, y):
+        x = float(x)
+        y = float(y)
+        x = self.getx() - x
+        y = self.gety() - y
+        
+        return math.hypot(x, y)
+        
+    def distance_from_point(self, point):
+        x = float(point._Point__x)
+        y = float(point._Point__y)
+        x = self.getx() - x
+        y = self.gety() - y
+        
+        return math.hypot(x, y)
+
+point1 = Point(0, 0)
+point2 = Point(1, 1)
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
