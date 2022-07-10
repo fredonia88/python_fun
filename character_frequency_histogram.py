4f886f5f5f8dda4bcccb2818e99fb9f43f7c0d2c
Frederick Jones <fjjones88@gmail.com>
2022-07-10 17:27:46 -0500

    Create character_frequency_histogram.py

---
+++ character_frequency_histogram.py
@@ -0,0 +1,23 @@
+from os import strerror
+
+file_location = input('provide file path to read:') #'/Users/frederick.jones/Desktop/character_frequency.txt'
+
+char_dict = {}
+
+try:
+    for line in open(file_location, 'rt'):
+        for ch in line:           
+            if ch.isalpha():
+                ch = ch.lower()
+                if ch in char_dict:
+                    char_dict[ch] += 1
+                else:
+                    char_dict[ch] = 1
+    file.close()
+except IOError as e:
+    print('I/O error occured:', strerror(e.errno))
+    exit(e.errno)
+    
+print('alpha counts in file:')
+for i in char_dict:
+    print(i + ' ->', char_dict[i])
